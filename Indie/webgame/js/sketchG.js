de0c162824e7637facff845464a8b92e7859e6c2
Maria Yala <ghost@Marias-MacBook-Pro.local>
2019-07-08 16:39:36 -0400

    Removed creatability - back to PoseNet

--- Indie/webgame/js/sketchG.js
+++ Indie/webgame/js/sketchG.js
@@ -6,9 +6,6 @@ var walls;
 var killers;
 var coins;
 let video;
-let poseNet;
-let noseX = 0;
-let noseY = 0;
 var player
 var floor;
 var pitchW = 640;
@@ -17,7 +14,6 @@ var pX = 0;
 var pY = 380; // Value to keep the paddle constant at y position
 var ball;
 var gameIsOver = false;
-var gameIsStarted = false;
 var score = 0
 var pSize = 100;
 var hits = 0;
@@ -29,8 +25,16 @@ function setup(){
 	canvas.style('display','block');
   video = createCapture(VIDEO);
   video.hide();
-  poseNet = ml5.poseNet(video, modelReady);
-  poseNet.on('pose', gotPoses);
+
+  // Create and initialize a pose input element
+  input = document.createElement('acc-pose-input');
+  input.initialize();
+
+  // Set its dimensions to match the dimensions of the canvas
+  input.contentElement = canvas.elt;
+
+  // Pick a body part to track
+  input.part = 'leftWrist';
 
   // Create the walls
   walls = new Group();
@@ -54,8 +58,8 @@ function setup(){
 
   // Create the ball
   ball = createSprite(random(30, pitchW), 30, 40, 40);
-  ball.shapeColor = color(0);
-  ball.velocity.y = 0;
+  ball.shapeColor = color(255);
+  ball.velocity.y = 2;
 
   // Create the killers group
   killers = new Group();
@@ -64,58 +68,46 @@ function setup(){
   coins = new Group();
 }
 
-function gotPoses(poses){
-  if(poses.length > 0){
-    let nX = poses[0].pose.keypoints[0].position.x;
-    let nY = poses[0].pose.keypoints[0].position.y;
-    noseX = lerp(noseX, nX, 0.5);
-    noseY = lerp(noseY, nY, 0.5);
-  }
-}
-
-function modelReady(){
-  console.log(ml5);
-  console.log('model ready');
-}
-
 function draw(){
-	background(255);
+	background(0);
+  // image(video, 0, 0);
+  // Make sure the pose input component has been loaded
+  if(input.isReady){
+    // Draw the camera input to the canvas
+    // canvas.elt.getContext('2d').drawImage(input.canvas, 0, 0,
+    // input.canvas.width, input.canvas.height);
 
-  tint(255, 63);
-  image(video, 0, 0, width, height);
+    // Get the position of the pointer
+    let x = input.targetPosition[0];
+    let y = input.targetPosition[1];
 
-  // Update the paddle y position so paddle follows player's wrist
-    pX = noseX;
-    player.velocity.x = (pX - player.position.x)*0.1;
-    // player.velocity.y = (mouseY - player.position.y)*0.1;
-    // player.position.x = pX;
-    player.position.y = 420;
+    // Update the paddle y position so paddle follows player's wrist
+    pX = x;
+  }
+  player.velocity.x = (pX - player.position.x)*0.1;
+  // player.velocity.y = (mouseY - player.position.y)*0.1;
+  player.position.y = 420;
+  player.collide(walls);
 
-    player.collide(walls);
+  ball.collide(walls, bounceDown);
+  ball.collide(player, bounceUp);
+  ball.collide(floor, gameOver);
 
-    ball.collide(walls, bounceDown);
-    ball.collide(player, bounceUp);
-    ball.collide(floor, gameOver);
+  killers.collide(player, killPlayer);
 
-    killers.collide(player, killPlayer);
+  coins.collide(player, getCoin);
+  coins.collide(floor, removeCoin);
 
-    coins.collide(player, getCoin);
-    coins.collide(floor, removeCoin);
+  drawSprites();
 
-    drawSprites();
-
-    fill(0);
-    textSize(25);
-    textAlign(CENTER, CENTER);
-    text("Score : " + score, width-100, 50);
-    textSize(72);
-    if(gameIsOver){
-      text("Game Over!", width/2, height/2);
-    }
-}
-
-function keyPressed(){
-  ball.velocity.y = 2;
+  fill(255);
+  textSize(25);
+  textAlign(CENTER, CENTER);
+  text("Score : " + score, width-100, 50);
+  textSize(72);
+  if(gameIsOver){
+    text("Game Over!", width/2, height/2);
+  }
 }
 
 function getCoin(coin){
@@ -144,27 +136,28 @@ function gameOver(ball, floor){
 
 function bounceUp(ball, player){
   ball.shapeColor = color(0, 255, 0);
+  player.shapeColor = color(255, 0, 0);
   ball.velocity.y *= -1;
 
   // Launch killers
   var kills = random(1,7); // random number of killers to make
   for(var i = 0; i <= kills; i++){
-    var spr = createSprite(random(20, width-20), random(20, 380), 15, 15);
-    spr.shapeColor = color(255, 0, 0);
+    var spr = createSprite(random(20, width-20), random(20, 380), 10, 10);
+    spr.shapeColor = color(255);
     spr.velocity.y = 1;
     killers.add(spr);
   }
 
   // Generate coins
-  var spr2 = createSprite(random(20, width-20), random(20, 380), 15, 15);
-  spr2.shapeColor = color(0, 255, 0);
+  var spr2 = createSprite(random(20, width-20), random(20, 380), 10, 10);
+  spr2.shapeColor = color(255, 255, 0);
   spr2.velocity.y = 2;
   coins.add(spr2);
 }
 
 function bounceDown(ball, wall){
   ball.velocity.y *= -1;
-  ball.shapeColor = color(0);
+  ball.shapeColor = color(255);
   ball.position.x = random(0, pitchW - 40);
 }
 
